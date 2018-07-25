4696bdfeb00b4e7a75f39ce0f98747d36b8a6c56
Hiroaki Oshima <kifa0422@gmail.com>
2018-07-25 12:22:26 -0700

    test server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log('GET FUNCTION WAS CALLED');
+    res.status = 200;
+    res.end('you have successfully get the data from the server');
+});
+
+app.listen(3000, () => {
+    console.log('server is running');
+})
